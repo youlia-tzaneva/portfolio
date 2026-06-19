18acc07d4f5d496e5449eec4ebab222b7cf1c0f1
youlia-tzaneva <youlia.shopping.tzaneva@gmail.com>
2026-06-19 20:51:00 +0300

    Updated CV link

---
+++ js/fonts-ready.js
@@ -0,0 +1,31 @@
+(function () {
+  var root = document.documentElement;
+
+  function reveal() {
+    root.classList.remove("fonts-loading");
+    root.classList.add("fonts-ready");
+  }
+
+  if (!document.fonts || !document.fonts.load) {
+    reveal();
+    return;
+  }
+
+  var criticalFonts = [
+    '400 16px "Domaine Display Narrow"',
+    '700 16px "Domaine Display Narrow"',
+    '400 16px CasualScript',
+    '400 16px Nunito',
+    '700 16px Nunito',
+  ];
+
+  Promise.all(
+    criticalFonts.map(function (spec) {
+      return document.fonts.load(spec).catch(function () {
+        return undefined;
+      });
+    })
+  ).then(reveal);
+
+  setTimeout(reveal, 2500);
+})();
